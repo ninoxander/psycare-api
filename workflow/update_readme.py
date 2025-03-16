0d993100687fe5585992260d11bb64d2f56770d8
ninoxander <za220111414@zapopan.tecmm.edu.mx>
2025-03-16 07:29:46 -0600

    Actualizando el update_readme.py para ser consecuente con la tabla en sheets

--- workflow/update_readme.py
+++ workflow/update_readme.py
@@ -1,29 +1,17 @@
 import pandas as pd
 import os
 
-base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 csv_path = os.path.join(base_dir, "data.csv")
 readme_path = os.path.join(base_dir, "README.md")
 
 df = pd.read_csv(csv_path)
 
-coverage_columns = df.columns[1:]
-
-def convert_to_numeric(value):
-    weights = {
-        "Basic": 25,
-        "Medium": 50,
-        "Full": 100
-    }
-    return weights.get(value, 0)
-
-df_numeric = df.copy()
-for col in coverage_columns:
-    df_numeric[col] = df[col].map(convert_to_numeric)
+total_implemented = df[df["IMPLEMENTED"] == "TRUE"].shape[0]
+total_endpoints = df.shape[0]
+percent_implemented = (total_implemented / total_endpoints) * 100
 
 df["% Coverage"] = df_numeric[coverage_columns].mean(axis=1).round(2)
-
-total_coverage = df["% Coverage"].mean().round(2)
 
 markdown_table = df.to_markdown(index=False)
 
@@ -32,7 +20,8 @@ with open(readme_path, "r") as file:
 
 start = readme.index("<!-- START_TABLE -->\n") + 1
 end = readme.index("<!-- END_TABLE -->\n")
-readme[start:end] = [markdown_table + "\n\n**Total Coverage: {}%**\n".format(total_coverage)]
+progress_bar = f"![](https://geps.dev/progress/{int(percent_implemented)})"
+readme[start:end] = [markdown_table + "\n\n**Total Coverage: {}%**\n{}".format(percent_implemented.round(2), progress_bar)]
 
 with open(readme_path, "w") as file:
     file.write("### API Coverage\n")
