564c451d9a60d66acc79123d8b7c3ed50ea9607f
ninoxander <za220111414@zapopan.tecmm.edu.mx>
2025-04-26 19:20:22 -0600

    Hot fix: modificando CORS para permitir acceso a swagger en deploy server

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require("cors"); 
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
 
 const { swaggerUi, specs } = require("./swagger");
 
 var authMiddleware = require("./utils/auth");
-var indexRouter = require('./routes/index');
-var signupRouter = require('./routes/signup');
-var loginRouter = require('./routes/login');
-var emotionalrecordsRouter = require('./routes/emotionalrecords');
+var indexRouter = require("./routes/index");
+var signupRouter = require("./routes/signup");
+var loginRouter = require("./routes/login");
+var emotionalrecordsRouter = require("./routes/emotionalrecords");
 var habitsRouter = require("./routes/habits");
 var notificationsRouter = require("./routes/notifications");
 var sesionsRouter = require("./routes/sesions");
 var articlesRouter = require("./routes/articles");
-var questionnariesRouter = require('./routes/questionnaries');
-var alertsRouter = require('./routes/alerts');
+var questionnariesRouter = require("./routes/questionnaries");
+var alertsRouter = require("./routes/alerts");
 var userSettingsRouter = require("./routes/user_settings");
 var userRouter = require("./routes/user");
 var reportsRouter = require("./routes/reports");
@@ -24,31 +24,38 @@ var testRouter = require("./routes/test");
 
 var app = express();
 
-app.use(cors());
-// Activar el bloque de abajo en prod
-/* app.use(cors({
-    origin: 'http://localhost:3001', // el frontend
-    credentials: true, // permitir credenciales (cookies, auth headers)
-}));
-*/
-app.use(logger('dev'));
+const allowedOrigins = ["http://192.168.56.1:3000/", "http://localhost:3001"];
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+                callback(null, true);
+            } else {
+                callback(new Error("No permitido por CORS"));
+            }
+        },
+    })
+);
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Endpoints disponibles
-app.use('/', indexRouter);
-app.use('/signup', signupRouter);
-app.use('/login', loginRouter);
-app.use('/records', emotionalrecordsRouter);
-app.use('/habits', habitsRouter);
-app.use('/notifications', notificationsRouter);
-app.use('/sessions', sesionsRouter);
-app.use('/articles', articlesRouter);
-app.use('/questionnaries', questionnariesRouter);
-app.use('/alerts', alertsRouter);
-app.use('/user-settings', userSettingsRouter);
+app.use("/", indexRouter);
+app.use("/signup", signupRouter);
+app.use("/login", loginRouter);
+app.use("/records", emotionalrecordsRouter);
+app.use("/habits", habitsRouter);
+app.use("/notifications", notificationsRouter);
+app.use("/sessions", sesionsRouter);
+app.use("/articles", articlesRouter);
+app.use("/questionnaries", questionnariesRouter);
+app.use("/alerts", alertsRouter);
+app.use("/user-settings", userSettingsRouter);
 app.use("/users", userRouter);
 app.use("/reports", reportsRouter);
 app.use("/test", testRouter);
